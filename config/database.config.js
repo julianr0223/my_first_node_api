51a5d18a45440332f5f6e4533fef7481194739b9
Julian Rincon <julian.vera@ceiba.com.co>
2020-04-11 13:06:58 -0500

    Se cambia nombre base de datos de prueba

--- config/database.config.js
+++ config/database.config.js
@@ -3,5 +3,5 @@ module.exports = {
     url: 'mongodb://localhost:27017/test_api_1',
 
     // url para conexion a mongo db atlas
-    url_Atlas: 'mongodb+srv://julianr0223:BiostaR0223@cluster0-ifzv3.mongodb.net/sample_geospatial?retryWrites=true&w=majority'
+    url_Atlas: 'mongodb+srv://julianr0223:BiostaR0223@cluster0-ifzv3.mongodb.net/api_rest1?retryWrites=true&w=majority'
 }
